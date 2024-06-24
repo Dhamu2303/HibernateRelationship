991ceb417efd2523a50e7d44f27199ca7f46b78c
Dhamodharan Ravichandran <162108469+Dhamu2303@users.noreply.github.com>
2024-06-24 10:48:36 +0530

    Add files via upload

---
+++ HibernateRelationship/src/str/Vender.java
@@ -0,0 +1,32 @@
+package str;
+
+import java.util.Set;
+
+@SuppressWarnings("rawtypes")
+public class Vender 
+{
+   private int vendorId;
+   private String vendorName;
+   private Set childern;
+   
+public int getVendorId() {
+	return vendorId;
+}
+public void setVendorId(int vendorId) {
+	this.vendorId = vendorId;
+}
+public String getVendorName() {
+	return vendorName;
+}
+public void setVendorName(String vendorName) {
+	this.vendorName = vendorName;
+}
+public Set getChildern() {
+	return childern;
+}
+public void setChildern(Set childern) {
+	this.childern = childern;
+}
+   
+   
+}
